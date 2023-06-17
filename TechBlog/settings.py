a70c79452c262b6f828d008203d15bc92ad6d9b7
Samyak Jain <samjain06@gmail.com>
2023-06-17 03:50:12 -0500

    Created Post and Comment model

--- TechBlog/settings.py
+++ TechBlog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+# For CST, TZ ID is: America/Chicago
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
