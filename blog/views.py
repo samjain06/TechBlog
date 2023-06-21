fac70021c910371ad3bcd71456cd7ca969f11a22
Samyak Jain <samjain06@gmail.com>
2023-06-20 23:45:08 -0500

    added comment function views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 from django.utils import timezone
 from django.urls import reverse_lazy
@@ -57,3 +58,43 @@ class DraftListView(LoginRequiredMixin, ListView):
         # '__lte' means less than or equal to. It is equivalent to WHERE clause in SQL query.
         # '-' in order_by arg means descending order. For ascending order we remove '-'
         return Post.objects.filter(published_date__isnull=True).order_by('-create_date')
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+# COMMENTS CLASSES
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    # post_pk is initialized because it will not remember content.pk  after content removed
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
