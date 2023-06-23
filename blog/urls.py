d5a60e416e3c2695f1959f509167725e68054e53
Samyak Jain <samjain06@gmail.com>
2023-06-23 18:28:34 -0500

    updated id to pk for comment

--- blog/urls.py
+++ blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
-    path('post/<int:id>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
