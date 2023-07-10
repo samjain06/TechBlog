7d0e12c9ec93950799d63751872b06d5d67e76c1
Samyak Jain <samjain06@gmail.com>
2023-07-09 23:19:44 -0500

    added PostgreSQL for Vercel

--- TechBlog/settings.py
+++ TechBlog/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / 'secrets.env')
 
 TEMPLATE_DIR = BASE_DIR / 'blog/templates/blog'
 
@@ -75,12 +79,26 @@ WSGI_APPLICATION = 'TechBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+if os.environ.get('VERCEL_ENV') == 'production':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('PRODUCTION_DB_URL'), conn_max_age=60
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DEVELOPMENT_DB_URL'), conn_max_age=60
+        )
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
